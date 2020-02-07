c5655d94415da05919706598ebf8e5ac42cb80b7
QAP28 <QAP28@QAP28-PC>
2020-02-07 16:09:29 +0545

    Encaps

---
+++ Encapsulation/src/pckg/Student.java
@@ -0,0 +1,12 @@
+package pckg;
+
+public class Student {
+	private String name2;  
+	public String getName(){  
+	return name2;  
+	}  
+	public void setName(String name){  
+	this.name2=name;  
+	}
+
+}
