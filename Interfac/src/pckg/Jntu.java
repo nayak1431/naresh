ea35b2ead47291ffcf7270eba8b2770707c2ecf8
QAP28 <QAP28@QAP28-PC>
2020-02-07 16:24:06 +0545

    interface

---
+++ Interfac/src/pckg/Jntu.java
@@ -0,0 +1,8 @@
+package pckg;
+
+public class Jntu {
+	int i=20;
+	void add() {
+	}
+
+}
---
+++ Interfac/src/pckg/Proginteger.java
@@ -0,0 +1,21 @@
+package pckg;
+
+public class Proginteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jntu20 rw1_20=new Jntu20();
+		rw1_20.add();
+		
+		Jntu21 rw1_21=new Jntu21();
+		rw1_21.add();
+		
+		Jntu20  obj=new Jntu20();
+		obj.add();
+		
+		Jntu21  obj1=new Jntu21();
+		obj1.add();
+
+	}
+
+}
