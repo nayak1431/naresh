a364d1357cfc7dc374f8fa6d3d70ea911bfff023
QAP28 <QAP28@QAP28-PC>
2020-02-07 15:39:09 +0545

    Calculation

---
+++ Calculation/src/pckg/Cls.java
@@ -0,0 +1,41 @@
+package pckg;
+
+public class Cls {
+	
+		
+	
+	  int a;
+	  int b;
+	  int t;
+	
+	  
+void add(){
+	int  i=5;
+	int   j=2;
+	 int t=i+j;
+	 System.out.println(t);
+}
+
+void sub(){
+	  a=5;
+	  b=2;
+	 int t=a-b;
+	 System.out.println(t);
+}
+
+void mul(){
+	  a=5;
+	  b=2;
+	 int t=a*b;
+	 System.out.println(t);
+}
+
+void add(int a,int b){
+	 
+	 int t=a+b;
+	 System.out.println(t);
+}
+
+
+}
+
