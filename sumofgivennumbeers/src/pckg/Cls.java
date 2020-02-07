bb3f289e19031bd54a4cf0c83cc613dcc31ad61c
QAP28 <QAP28@QAP28-PC>
2020-02-07 14:27:20 +0545

    ADD NUMBERS

---
+++ sumofgivennumbeers/src/pckg/Cls.java
@@ -0,0 +1,16 @@
+package pckg;
+
+public class Cls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1; i<=10; i++)
+		{
+			sum = sum + i;
+		}
+		System.out.println("The Sum of Given Number is ::: " +sum);
+
+	}
+
+}
