d5004f4c415b638a2875a14f2d75ac8eacd78f05
QAP28 <QAP28@QAP28-PC>
2020-02-07 15:45:16 +0545

    ScanPrint

---
+++ ScanPrint/src/pckg/Cls.java
@@ -0,0 +1,17 @@
+package pckg;
+
+import java.util.Scanner;
+
+public class Cls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		System.out.println("enter your value");
+		Scanner sc=new Scanner(System.in);
+		 a=sc.nextInt();
+		System.out.println("Entered your value"+a);
+
+	}
+
+}
